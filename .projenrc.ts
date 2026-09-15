14b7e696d028d1eb1d44173d5a8e0a21b7cdb3a8
yicr <yicr@users.noreply.github.com>
2026-09-15 08:52:27 +0000

    chore: migrate projenrc to ProjenCdkConstructLibrary

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,46 +1,18 @@
-import { awscdk, javascript, github } from 'projen';
-const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'yicr',
-  authorAddress: 'yicr@users.noreply.github.com',
-  cdkVersion: '2.232.0',
-  defaultReleaseBranch: 'main',
-  typescriptVersion: '6.0.x',
-  jsiiVersion: '6.0.x',
-  name: 'rds-aurora-bootstrapper',
-  packageManager: javascript.NodePackageManager.NPM,
+import { ProjenCdkConstructLibrary } from '@gammarers/projen-projects';
+import { awscdk } from 'projen';
+const project = new ProjenCdkConstructLibrary({
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/gammarers-aws-cdk-constructs/rds-aurora-bootstrapper.git',
+  releaseToNpm: true,
+  npmTrustedPublishing: true,
+  cdkVersion: '2.232.0',
+  name: 'rds-aurora-bootstrapper',
+  repository: 'https://github.com/gammarers-aws-cdk-constructs/rds-aurora-bootstrapper.git',
   devDeps: [
+    '@gammarers/projen-projects@^0.2.4',
     '@aws-sdk/client-rds-data@^3.743.0',
     '@aws-sdk/client-secrets-manager@^3.743.0',
     '@types/aws-lambda@^8.10.161',
   ],
-  releaseToNpm: true,
-  npmTrustedPublishing: true,
-  npmAccess: javascript.NpmAccess.PUBLIC,
-  minNodeVersion: '20.0.0',
-  workflowNodeVersion: '24.x',
-  depsUpgradeOptions: {
-    workflowOptions: {
-      labels: ['auto-approve', 'auto-merge'],
-      schedule: javascript.UpgradeDependenciesSchedule.WEEKLY,
-    },
-  },
-  githubOptions: {
-    projenCredentials: github.GithubCredentials.fromApp({
-      permissions: {
-        pullRequests: github.workflows.AppPermission.WRITE,
-        contents: github.workflows.AppPermission.WRITE,
-        workflows: github.workflows.AppPermission.WRITE,
-      },
-    }),
-  },
-  autoApproveOptions: {
-    allowedUsernames: [
-      'gammarers-projen-upgrade-bot[bot]',
-      'yicr',
-    ],
-  },
   jestOptions: {
     extraCliOptions: ['--silent'],
   },
